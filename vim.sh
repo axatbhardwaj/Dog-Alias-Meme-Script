91e3f5db4e7e359bd1804c2b6475641acefe3d10
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-08-23 12:56:06 +0530

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -1,3 +1,2 @@
 echo 'alias dog=vim' >> ~/.bashrc
-
-echo 'This appends the alias to ~/.bashrc. Restart if it doesn't work'
+echo 'This appends the alias to ~/.bashrc. Restart bash(just restart your pc) if it doesn't work'
