2e589256da4eea44cf4d246f013abda9ef4a4a60
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-08-23 12:56:59 +0530

    Update code-insiders.sh

--- code-insiders.sh
+++ code-insiders.sh
@@ -1,2 +1,2 @@
 echo 'alias dog=code-insiders' >> ~/.bashrc
-echo 'This appends the alias to ~/.bashrc, to verify, go to the file'
+echo 'This appends the alias to ~/.bashrc. Restart bash(just restart your pc) if it doesn't work'
