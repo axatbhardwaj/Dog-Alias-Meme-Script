6b102594fc5391e1200b7b2f56c318059e1c0268
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-08-23 08:23:07 +0530

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,1 +1,8 @@
-#sudo alias <your text editor>="dog"
+#echo 'alias dog=nano' >> ~/.bashrc
+#echo 'This appends the alias to ~/.bashrc, to verify, go to the file'
+
+
+
+
+#After editing the command, fork this project, and edit this file. Click on raw, copy link, and append that link to my installer command below:
+#
